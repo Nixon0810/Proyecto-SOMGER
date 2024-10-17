f2b9b03fb8a37f46b14c8f13bd65b04788ba5425
Nixon1901 <nixon25herrera@gmail.com>
2024-10-17 11:49:47 -0500

    Se modifica SP: VentasPorAñoYSucursal

--- USP_VentasPorAnoYSucursal_SEL.sql
+++ USP_VentasPorAnoYSucursal_SEL.sql
@@ -1,5 +1,10 @@
 use SCOMGER
 Go
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[USP_VentasPorAnoYSucursal_SEL]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[USP_VentasPorAnoYSucursal_SEL]
+GO
+
 CREATE PROCEDURE USP_VentasPorAnoYSucursal_SEL
     @MesInicio int,
     @MesFin int,
@@ -9,13 +14,15 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT D.IdSucursal, DS.Descripcion, SUM(D.CantidadVendida) AS TotalCantidadVendida, SUM(D.SubTotal) AS TotalSubTotal
+    SELECT	DS.Descripcion AS 'x', 
+			SUM(D.SubTotal) AS 'y',
+			DS.Descripcion+' '+CAST(SUM(D.SubTotal) AS Varchar(20)) AS 'Text'
     FROM Datos D
-    INNER JOIN DimSucursal DS ON D.IdSucursal = DS.IdSucursal
-    INNER JOIN DimTiempo DT ON D.IdTiempo = DT.IdTiempo
-    WHERE (D.IdSucursal = @IdSucursal OR @IdSucursal IS NULL)
-      AND DT.Mes BETWEEN @MesInicio AND @MesFin
-      AND DT.Ano = @Ano
-    GROUP BY D.IdSucursal, DS.Descripcion;
+		INNER JOIN DimSucursal DS ON D.IdSucursal = DS.IdSucursal
+		INNER JOIN DimTiempo DT ON D.IdTiempo = DT.IdTiempo
+   /* WHERE (D.IdSucursal = @IdSucursal OR @IdSucursal IS NULL)
+		AND DT.Mes BETWEEN @MesInicio AND @MesFin
+		AND DT.Ano = @Ano*/
+    GROUP BY DS.Descripcion;
 END
 GO
